b788bf7568ada6581b890fa6d4a6061ed68d98fd
Luke Redpath <luke@lukeredpath.co.uk>
2011-09-08 01:52:18 +0100

    Put async examples in a root context

--- Examples/ExampleAsyncSpec.m
+++ Examples/ExampleAsyncSpec.m
@@ -16,78 +16,80 @@ SPEC_BEGIN(ExampleAsyncSpec)
 
 const float nanosecondToSeconds = 1e9;
 
-it(@"should verify asynchronous expectations on a variable that starts as nil that succeed in time", ^{
-  __block NSString *fetchedData = nil;
-
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-    fetchedData = @"expected response data";
+context(@"Asynchronous specs", ^{
+  it(@"should verify asynchronous expectations on a variable that starts as nil that succeed in time", ^{
+    __block NSString *fetchedData = nil;
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+      fetchedData = @"expected response data";
+    });
+    
+    // this will block until the matcher is satisfied or it times out (default: 1s)
+    [[theObject(&fetchedData) shouldEventually] equal:@"expected response data"];
   });
-
-  // this will block until the matcher is satisfied or it times out (default: 1s)
-  [[theObject(&fetchedData) shouldEventually] equal:@"expected response data"];
-});
-
-it(@"should verify asynchronous expectations on a variable that starts as nil that succeed with an explicit time", ^{
-  __block NSString *fetchedData = nil;
-
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-    fetchedData = @"expected response data";
+  
+  it(@"should verify asynchronous expectations on a variable that starts as nil that succeed with an explicit time", ^{
+    __block NSString *fetchedData = nil;
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+      fetchedData = @"expected response data";
+    });
+    
+    // this will block until the matcher is satisfied or it times out (default: 1s)
+    [[theObject(&fetchedData) shouldEventuallyBeforeTimingOutAfter(2.0)] equal:@"expected response data"];
   });
-
-  // this will block until the matcher is satisfied or it times out (default: 1s)
-  [[theObject(&fetchedData) shouldEventuallyBeforeTimingOutAfter(2.0)] equal:@"expected response data"];
-});
-
-it(@"should verify asynchronous expectations on the return value of a block", ^{
-  __block NSString *fetchedData = nil;
-
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-    fetchedData = @"expected response data";
+  
+  it(@"should verify asynchronous expectations on the return value of a block", ^{
+    __block NSString *fetchedData = nil;
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+      fetchedData = @"expected response data";
+    });
+    
+    [[expectFutureValue([fetchedData uppercaseString]) shouldEventually] equal:@"EXPECTED RESPONSE DATA"];
   });
-
-  [[expectFutureValue([fetchedData uppercaseString]) shouldEventually] equal:@"EXPECTED RESPONSE DATA"];
-});
-
-it(@"should verify asynchronous mock expectations on an existing object set before the asynchronous call", ^{
-  __block id mock = [KWMock mockForClass:[NSString class]];
-
-  [[[mock shouldEventually] receive] uppercaseString];
-
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-    [mock uppercaseString];
+  
+  it(@"should verify asynchronous mock expectations on an existing object set before the asynchronous call", ^{
+    __block id mock = [KWMock mockForClass:[NSString class]];
+    
+    [[[mock shouldEventually] receive] uppercaseString];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+      [mock uppercaseString];
+    });
   });
-});
-
-it(@"should verify asynchronous mock expectations on an existing object set after the asynchronous call", ^{
-  __block id mock = [KWMock mockForClass:[NSString class]];
-
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-    [mock uppercaseString];
+  
+  it(@"should verify asynchronous mock expectations on an existing object set after the asynchronous call", ^{
+    __block id mock = [KWMock mockForClass:[NSString class]];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+      [mock uppercaseString];
+    });
+    
+    [[[mock shouldEventually] receive] uppercaseString];
   });
-
-  [[[mock shouldEventually] receive] uppercaseString];
-});
-
-it(@"should verify asynchronous expectations on a variable that starts as nil and becomes not-nil", ^{
-  __block NSString *fetchedData = nil;
-
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-    fetchedData = @"expected response data";
+  
+  it(@"should verify asynchronous expectations on a variable that starts as nil and becomes not-nil", ^{
+    __block NSString *fetchedData = nil;
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+      fetchedData = @"expected response data";
+    });
+    
+    // this will block until the matcher is satisfied or it times out (default: 1s)
+    [[theObject(&fetchedData) shouldEventually] beNonNil];
   });
-
-  // this will block until the matcher is satisfied or it times out (default: 1s)
-  [[theObject(&fetchedData) shouldEventually] beNonNil];
-});
-
-it(@"should verify asynchronous expectations on a variable that starts as non-nil and becomes nil", ^{
-  __block NSString *fetchedData = @"not nil";
-
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-    fetchedData = nil;
+  
+  it(@"should verify asynchronous expectations on a variable that starts as non-nil and becomes nil", ^{
+    __block NSString *fetchedData = @"not nil";
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * nanosecondToSeconds), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+      fetchedData = nil;
+    });
+    
+    // this will block until the matcher is satisfied or it times out (default: 1s)
+    [[theObject(&fetchedData) shouldEventually] beNil];
   });
-
-  // this will block until the matcher is satisfied or it times out (default: 1s)
-  [[theObject(&fetchedData) shouldEventually] beNil];
 });
 
 SPEC_END
