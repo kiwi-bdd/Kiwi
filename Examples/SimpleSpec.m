6ec09c63928b57567064680f0a7738abdf9e8575
Luke Redpath <luke@lukeredpath.co.uk>
2011-10-19 01:02:23 +0100

    Added some comprehensive examples of how before each/all hooks work and the order of execution.

--- Examples/SimpleSpec.m
+++ Examples/SimpleSpec.m
@@ -37,21 +37,74 @@ describe(@"stack", ^{
     });
 });
 
-describe(@"beforeAll", ^{
+SPEC_END
+
+SPEC_BEGIN(HooksBehaviour)
+
+describe(@"before hooks behaviour", ^{
   __block NSInteger calls = 0;
   
   beforeAll(^{
     calls++;
   });
   
-  it(@"will be called before this spec", ^{
-    [[theValue(calls) should] equal:theValue(1)];
-	});
-  
-  it(@"will not be called again before this spec", ^{
-    [[theValue(calls) should] equal:theValue(1)];
+  beforeEach(^{
+    calls++;
   });
   
+  it(@"will call beforeAll and beforeEach before this spec", ^{
+    [[theValue(calls) should] equal:theValue(2)];
+	});
+  
+  it(@"will call beforeEach but not beforeAll before this spec", ^{
+    [[theValue(calls) should] equal:theValue(3)];
+  });
+  
+  context(@"with nested contexts", ^{
+    beforeAll(^{
+      calls++;
+    });
+    
+    beforeEach(^{
+      calls++;
+    });
+    
+    it(@"will call inner beforeAll and inner and outer beforeEach before this spec", ^{
+      [[theValue(calls) should] equal:theValue(6)];
+    });
+    
+    it(@"will call inner and outer beforeEach but neither beforeAll before this spec", ^{
+      [[theValue(calls) should] equal:theValue(8)];
+    });
+    
+    context(@"and another", ^{
+      beforeAll(^{
+        calls++;
+      });
+      
+      beforeEach(^{
+        calls++;
+      });
+      
+      it(@"will call inner beforeAll and all inner and outer beforeEach hooks before this spec", ^{
+        [[theValue(calls) should] equal:theValue(12)];
+      });
+      
+      it(@"will call all inner and outer beforeEach but none of the beforeAll hooks before this spec", ^{
+        [[theValue(calls) should] equal:theValue(15)];
+      });
+      
+    });
+    
+    it(@"will call this after the nested context specs above", ^{
+	    [[theValue(calls) should] equal:theValue(17)];
+    });
+  });
+  
+  it(@"will call this after both nested context specs above", ^{
+    [[theValue(calls) should] equal:theValue(18)];
+  });
 });
 
 SPEC_END
+
