a9930cdaf9b95af3657231c50887dd3cfe035154
Sergej Tatarincev <infernal89@gmail.com>
2012-02-21 14:06:44 +0200

    Added Xcode 4 template for Kiwi spec
    Spec file template and script, that installs template to
    ~/Library/Developer/Xcode/Templates

---
+++ Templates/Kiwi Spec.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___)
+
+SPEC_END
+
+
