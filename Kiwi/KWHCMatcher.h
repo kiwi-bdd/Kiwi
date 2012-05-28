b027c24719668cdb424b50229f57a9e3ef235d75
Paul Zabelin <paul@blazingcloud.net>
2012-05-28 13:34:30 -0700

    Fix hamcrest matcher to report failures with subject add note about using OCHamcrestIOS framework with Kiwi

--- Kiwi/KWHCMatcher.h
+++ Kiwi/KWHCMatcher.h
@@ -3,6 +3,14 @@
  * to support Hamcrest integration with Kiwi.
  *
  * The Hamcrest project can be found here: http://code.google.com/p/hamcrest/
+ *
+ * NOTE: When using Hamcrest library with Kiwi include Hamcrest headers first in your specs
+ *       to use proper HCMatcher protocol definition instead of this minimal version:
+ *       #import <OCHamcrestIOS/OCHamcrestIOS.h>
+ *       #import "Kiwi.h"
+ *       See http://code.google.com/p/hamcrest/source/browse/trunk/hamcrest-objectivec/Source/Core/HCMatcher.h
+ *       Failure to do so might cause endless recursion in -[HCBaseDescription appendDescriptionOf:] as it checks if
+ *       HCMatcher conforms to HCSelfDescribing protocol, which is missing in this minimal version
  */
 
 #import <Foundation/Foundation.h>
