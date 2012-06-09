a7c4f5ae05419026a0a775f9a83957245d9a7f4a
Paul Zabelin <paul@blazingcloud.net>
2012-06-09 12:22:53 -0700

    fix warning of duplicate HCMatcher, warn when HCMatcher is not from hamcrest

--- Kiwi/KWHCMatcher.h
+++ Kiwi/KWHCMatcher.h
@@ -7,6 +7,14 @@
 
 #import <Foundation/Foundation.h>
 
+#if !defined(HCMatcher)
+
 @protocol HCMatcher <NSObject>
 - (BOOL)matches:(id)item;
 @end
+
+#else
+    #if !defined(HCSelfDescribing)
+        #warning defined HCMatcher does not seem to be a hamcrest HCMatcher as it should implement HCSelfDescribing
+    #endif
+#endif
