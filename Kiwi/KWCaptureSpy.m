2986d1b8ab763820af2c1e84801e0c3c7e9bcde0
modocache <modocache@gmail.com>
2012-11-15 10:44:06 -0500

    Remove trailing whitespace.

--- Kiwi/KWCaptureSpy.m
+++ Kiwi/KWCaptureSpy.m
@@ -17,7 +17,7 @@
 
 - (id)argument {
     if (!_argumentCaptured) {
-        @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:@"Argument requested has yet to be captured." userInfo:nil];    
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:@"Argument requested has yet to be captured." userInfo:nil];
     }
     return [[_argument retain] autorelease];
 }
@@ -25,7 +25,7 @@
 - (void)object:(id)anObject didReceiveInvocation:(NSInvocation *)anInvocation {
     if (!_argumentCaptured) {
         NSMethodSignature *signature = [anInvocation methodSignature];
-        const char *objCType = [signature messageArgumentTypeAtIndex:_argumentIndex];        
+        const char *objCType = [signature messageArgumentTypeAtIndex:_argumentIndex];
         if (KWObjCTypeIsObject(objCType)) {
             id argument = nil;
             [anInvocation getMessageArgument:&argument atIndex:_argumentIndex];
@@ -33,7 +33,7 @@
                 _argument = [argument copy];
             } else {
                 _argument = [argument retain];
-            }                
+            }
         } else {
             NSData *data = [anInvocation messageArgumentDataAtIndex:_argumentIndex];
             _argument = [[KWValue valueWithBytes:[data bytes] objCType:objCType] retain];
