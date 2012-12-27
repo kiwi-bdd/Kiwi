7b2838de2d63a6279aee95df7427534cb612fa78
Jason Felice <jason.m.felice@gmail.com>
2012-12-27 13:58:32 -0500

    Introduce KWMockDescription

---
+++ Kiwi/KWMockDescription.h
@@ -0,0 +1,21 @@
+//
+// Licensed under the terms in License.txt
+//
+// Copyright 2012 Allen Ding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KWMockDescription : NSObject {
+@private
+    BOOL isNullMock;
+    NSString *name;
+    Class mockedClass;
+    Protocol *mockedProtocol;
+}
+@property (nonatomic, readonly) BOOL isNullMock;
+@property (nonatomic, readonly) NSString *name;
+@property (nonatomic, readonly) Class mockedClass;
+@property (nonatomic, readonly) Protocol *mockedProtocol;
+@end
+
---
+++ Kiwi/KWMockDescription.m
@@ -0,0 +1,8 @@
+#import "KWMockDescription.h"
+
+@implementation KWMockDescription
+@synthesize isNullMock;
+@synthesize name;
+@synthesize mockedClass;
+@synthesize mockedProtocol;
+@end
