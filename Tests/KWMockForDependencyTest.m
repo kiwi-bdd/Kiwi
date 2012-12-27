de76fa2c19e7b8513f6092377f8836c3278ca308
Jason Felice <jason.m.felice@gmail.com>
2012-12-27 10:20:28 -0500

    Introduce KWMockForDependency

---
+++ Tests/KWMockForDependencyTest.m
@@ -0,0 +1,21 @@
+//
+// Licensed under the terms in License.txt
+//
+// Copyright 2012 Allen Ding. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import "KiwiTestConfiguration.h"
+#import "TestClasses.h"
+
+#if KW_TESTS_ENABLED
+
+@interface KWMockForDependencyTest : SenTestCase
+
+@end
+
+@implementation KWMockForDependencyTest
+
+@end
+
+#endif // #if KW_TESTS_ENABLED
