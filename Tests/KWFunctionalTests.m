a71381d736d4427b7e40b6213e251bc588d4eacc
Marin Usalj <mneorr@gmail.com>
2013-01-24 12:47:15 -0800

    improved spec. Tests for #190

--- Tests/KWFunctionalTests.m
+++ Tests/KWFunctionalTests.m
@@ -8,49 +8,60 @@
 
 #import "Kiwi.h"
 #import "KiwiTestConfiguration.h"
-#import "TestClasses.h"
 
-#if KW_TESTS_ENABLED
+static BOOL tests_were_run = NO;
 
-@interface KWFunctionalTests : SenTestCase
-@end
+SPEC_BEGIN(Functional)
 
-@implementation KWFunctionalTests
+NSMutableArray *calls = @[@"OuterTestCase",
+@"BeforeAll", @"AfterAll", @"BeforeEach", @"AfterEach", @"TestCase",
+@"InnerBeforeAll", @"InnerAfterAll", @"InnerBeforeEach", @"InnerAfterEach", @"InnerTestCase",
+@"DoubleInnerBeforeAll", @"DoubleInnerAfterAll", @"DoubleInnerBeforeEach", @"DoubleInnerAfterEach", @"DoubleInnerTestCase"
+].mutableCopy;
 
-- (void)testInnerContextsAreExecuted {
-    NSMutableArray *calls = @[@"OuterTestCase",
-    @"BeforeAll", @"AfterAll", @"BeforeEach", @"AfterEach", @"TestCase",
-    @"InnerBeforeAll", @"InnerAfterAll", @"InnerBeforeEach", @"InnerAfterEach", @"InnerTestCase",
-    @"DoubleInnerBeforeAll", @"DoubleInnerAfterAll", @"DoubleInnerBeforeEach", @"DoubleInnerAfterEach", @"DoubleInnerTestCase"
-    ].mutableCopy;
-    
-    describe(@"OuterDescribe", ^{
-        it(@"OuterTestCase", ^{ [calls removeObject:@"OuterTestCase"]; });
-        context(@"OuterContext", ^{
-            beforeAll(^{ [calls removeObject:@"BeforeAll"]; });
-            afterAll(^{ [calls removeObject:@"AfterAll"]; });
-            beforeEach(^{ [calls removeObject:@"BeforeEach"]; });
-            afterEach(^{ [calls removeObject:@"AfterEach"]; });
-            it(@"TestCase", ^{ [calls removeObject:@"TestCase"]; });
-            context(@"Context", ^{
-                beforeAll(^{ [calls removeObject:@"InnerBeforeAll"]; });
-                afterAll(^{ [calls removeObject:@"InnerAfterAll"]; });
-                beforeEach(^{ [calls removeObject:@"InnerBeforeEach"]; });
-                afterEach(^{ [calls removeObject:@"InnerAfterEach"]; });
-                it(@"InnerTestCase", ^{ [calls removeObject:@"InnerTestCase"]; });
-                context(@"InnerContext", ^{
-                    beforeAll(^{ [calls removeObject:@"DoubleInnerBeforeAll"]; });
-                    afterAll(^{ [calls removeObject:@"DoubleInnerAfterAll"]; });
-                    beforeEach(^{ [calls removeObject:@"DoubleInnerBeforeEach"]; });
-                    afterEach(^{ [calls removeObject:@"DoubleInnerAfterEach"]; });
-                    it(@"DoubleInnerTestCase", ^{ [calls removeObject:@"DoubleInnerTestCase"]; });
-                });
+describe(@"OuterDescribe", ^{
+    it(@"OuterTestCase", ^{ [calls removeObject:@"OuterTestCase"]; });
+    context(@"OuterContext", ^{
+        beforeAll(^{ [calls removeObject:@"BeforeAll"]; });
+        afterAll(^{ [calls removeObject:@"AfterAll"]; });
+        beforeEach(^{ [calls removeObject:@"BeforeEach"]; });
+        afterEach(^{ [calls removeObject:@"AfterEach"]; });
+        it(@"TestCase", ^{ [calls removeObject:@"TestCase"]; });
+        context(@"Context", ^{
+            beforeAll(^{ [calls removeObject:@"InnerBeforeAll"]; });
+            afterAll(^{ [calls removeObject:@"InnerAfterAll"]; });
+            beforeEach(^{ [calls removeObject:@"InnerBeforeEach"]; });
+            afterEach(^{ [calls removeObject:@"InnerAfterEach"]; });
+            it(@"InnerTestCase", ^{ [calls removeObject:@"InnerTestCase"]; });
+            context(@"InnerContext", ^{
+                beforeAll(^{ [calls removeObject:@"DoubleInnerBeforeAll"]; });
+                afterAll(^{ [calls removeObject:@"DoubleInnerAfterAll"]; });
+                beforeEach(^{ [calls removeObject:@"DoubleInnerBeforeEach"]; });
+                afterEach(^{ [calls removeObject:@"DoubleInnerAfterEach"]; });
+                it(@"DoubleInnerTestCase", ^{ [calls removeObject:@"DoubleInnerTestCase"]; });
             });
         });
     });
-    STAssertEquals(0, calls.count, @"Not all the blocks were called! Leftovers: %@", calls);
+});
+
+describe(@"Check", ^{
+    it(@"All the blocks were called", ^{
+        [[calls should] haveCountOf:0];
+        tests_were_run = YES;
+    });    
+});
+
+SPEC_END
+
+
+#if KW_TESTS_ENABLED
+@interface KWFunctionalTests : SenTestCase
+@end
+@implementation KWFunctionalTests
+
+- (void)testSuiteWasExecuted {
+    STAssertEquals(YES, tests_were_run, @"Test suite hasn't run!");
 }
 
 @end
-
 #endif // #if KW_TESTS_ENABLED
