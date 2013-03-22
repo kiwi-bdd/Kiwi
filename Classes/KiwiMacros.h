93b92edc097630ed7265d73e1d341f0dddc6d547
Adam Sharp <adam.sharp@outware.com.au>
2013-03-22 19:57:29 +1100

    Add thePointerValue() macro
    Allows for pointer comparisons via NSValue.

--- Classes/KiwiMacros.h
+++ Classes/KiwiMacros.h
@@ -60,6 +60,9 @@
         })
 #endif // #if defined(__GNUC__)
 
+// Allows for comparision of pointer values in expectations
+#define thePointerValue(expr) [NSValue valueWithPointer:(expr)]
+
 // Example group declarations.
 #define SPEC_BEGIN(name) \
     \
