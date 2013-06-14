9e7a20762f1bf1681139adaab5473a4c561dd950
Stepan Hruda <stepan@shopkeep.com>
2013-06-14 02:59:23 -0400

    Convert KWAfterEachNode to ARC

--- Classes/KWAfterEachNode.m
+++ Classes/KWAfterEachNode.m
@@ -12,7 +12,7 @@
 #pragma mark - Initializing
 
 + (id)afterEachNodeWithCallSite:(KWCallSite *)aCallSite block:(KWVoidBlock)aBlock {
-    return [[[self alloc] initWithCallSite:aCallSite description:nil block:aBlock] autorelease];
+    return [[self alloc] initWithCallSite:aCallSite description:nil block:aBlock];
 }
 
 #pragma mark - Accepting Visitors
