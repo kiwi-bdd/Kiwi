f8c202b8f7c24363987804216b3a0211a7d9dc29
Charles Augustine <charles@concentricsky.com>
2013-09-26 17:26:58 -0700

    Changing the KWCaptureSpy to a NonARC file to address its apparent inability to properly retain the argument it is capturing (as demonstrated in commit 733dcf18b177fa4fe69e5247ba74f5847b140d3a).

--- Classes/Core/KWCaptureSpy.m
+++ Classes/Core/KWCaptureSpy.m
@@ -1,54 +1,70 @@
 #import "KWCaptureSpy.h"
 
 #import "KWObjCUtilities.h"
+#import "KWNull.h"
 #import "KWValue.h"
 #import "NSInvocation+KiwiAdditions.h"
 #import "NSMethodSignature+KiwiAdditions.h"
 
 @interface KWCaptureSpy()
 
-@property (nonatomic, assign) BOOL argumentCaptured;
-@property (nonatomic, assign) NSUInteger argumentIndex;
 @property (nonatomic, strong) id argument;
 
 @end
 
-@implementation KWCaptureSpy
+@implementation KWCaptureSpy {
+	NSUInteger _argumentIndex;
+}
 
 - (id)initWithArgumentIndex:(NSUInteger)index {
     self = [super init];
     if (self) {
         _argumentIndex = index;
-        _argumentCaptured = NO;
     }
     return self;
 }
 
 - (id)argument {
-    if (!_argumentCaptured) {
+    if (!_argument) {
         @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:@"Argument requested has yet to be captured." userInfo:nil];
     }
-    return _argument;
+
+	if(_argument == [KWNull null]) {
+		return nil;
+	}
+	else {
+		return [[_argument retain] autorelease];
+	}
 }
 
 - (void)object:(id)anObject didReceiveInvocation:(NSInvocation *)anInvocation {
-    if (!_argumentCaptured) {
+    if (!_argument) {
         NSMethodSignature *signature = [anInvocation methodSignature];
         const char *objCType = [signature messageArgumentTypeAtIndex:_argumentIndex];
         if (KWObjCTypeIsObject(objCType) || KWObjCTypeIsClass(objCType)) {
             id argument = nil;
             [anInvocation getMessageArgument:&argument atIndex:_argumentIndex];
             if (KWObjCTypeIsBlock(objCType)) {
-                self.argument = [argument copy];
+                _argument = [argument copy];
             } else {
-                self.argument = argument;
+				if(argument == nil) {
+					_argument = [[KWNull null] retain];
+				}
+				else {
+					_argument = [argument retain];
+				}
             }
         } else {
             NSData *data = [anInvocation messageArgumentDataAtIndex:_argumentIndex];
-            _argument = [KWValue valueWithBytes:[data bytes] objCType:objCType];
+            _argument = [[KWValue valueWithBytes:[data bytes] objCType:objCType] retain];
         }
-        _argumentCaptured = YES;
     }
+}
+
+
+- (void)dealloc {
+    [_argument release];
+    [super dealloc];
 }
 
 @end
