1c44a6b7e3ff5074af5c910b5457aee27490ec59
Paul Zabelin <pzabelin+git@gmail.com>
2015-08-23 02:46:13 -0700

    allow use of notification matcher with async verifier in constructs like [[@"my notification" shouldEventually] bePosted]; or even shouldNotEventually by keeping observer alive when evaluted multiple times until first notification received

--- Classes/Matchers/KWNotificationMatcher.h
+++ Classes/Matchers/KWNotificationMatcher.h
@@ -18,4 +18,6 @@ typedef void (^PostedNotificationBlock)(NSNotification* note);
 - (void)bePostedWithObject:(id)object userInfo:(NSDictionary *)userInfo;
 - (void)bePostedEvaluatingBlock:(PostedNotificationBlock)block;
 
+#pragma mark - KWMatching
+@property (nonatomic, assign) BOOL willEvaluateMultipleTimes;
 @end
