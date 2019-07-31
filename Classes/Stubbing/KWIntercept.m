6224bee6cfc0edbb588870e44de0c9d88090f7c7
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-07-31 18:08:42 +0300

    fix supportsSecureCoding for intercepted classes

--- Classes/Stubbing/KWIntercept.m
+++ Classes/Stubbing/KWIntercept.m
@@ -37,6 +37,8 @@ void KWInterceptedForwardInvocation(id anObject, SEL aSelector, NSInvocation* an
 void KWInterceptedDealloc(id anObject, SEL aSelector);
 Class KWInterceptedClass(id anObject, SEL aSelector);
 Class KWInterceptedSuperclass(id anObject, SEL aSelector);
+BOOL KWInterceptedSupportsSecureCodingTrue(id anObject, SEL aSelector);
+BOOL KWInterceptedSupportsSecureCodingFalse(id anObject, SEL aSelector);
 
 #pragma mark - Getting Forwarding Implementations
 
@@ -119,6 +121,15 @@ Class KWInterceptClassForCanonicalClass(Class canonicalClass) {
 
     Class interceptMetaClass = object_getClass(interceptClass);
     class_addMethod(interceptMetaClass, @selector(forwardInvocation:), (IMP)KWInterceptedForwardInvocation, "v@:@");
+
+    SEL supportsSecureCodingSelector = @selector(supportsSecureCoding);
+    Method supportsSecureCodingMethod = class_getClassMethod(canonicalClass, supportsSecureCodingSelector);
+    if (supportsSecureCodingMethod != NULL) {
+        BOOL support = [(id)canonicalClass performSelector:supportsSecureCodingSelector];
+        IMP supportsSecureCodingIMP = support ? (IMP)KWInterceptedSupportsSecureCodingTrue
+                                              : (IMP)KWInterceptedSupportsSecureCodingFalse;
+        class_addMethod(interceptMetaClass, supportsSecureCodingSelector, supportsSecureCodingIMP, "B@:");
+    }
 
     return interceptClass;
 }
@@ -221,6 +232,14 @@ Class KWInterceptedSuperclass(id anObject, SEL aSelector) {
     Class originalClass = class_getSuperclass(interceptClass);
     Class originalSuperclass = class_getSuperclass(originalClass);
     return originalSuperclass;
+}
+
+BOOL KWInterceptedSupportsSecureCodingTrue(id anObject, SEL aSelector) {
+    return YES;
+}
+
+BOOL KWInterceptedSupportsSecureCodingFalse(id anObject, SEL aSelector) {
+    return NO;
 }
 
 #pragma mark - Managing Objects Stubs
